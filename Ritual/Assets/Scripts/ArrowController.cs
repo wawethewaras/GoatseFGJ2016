57692b6b116469bb1e8c0e557b81d719108a0757
wawethewaras <wawe212@hotmail.fi>
2016-01-29 20:06:39 +0200

    jee

---
+++ Ritual/Assets/Scripts/ArrowController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowController : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other) {
+
+    }
+}
---
+++ Ritual/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject[] locations;
+    int currentScene = 0;
+
+
+    void Start()
+    {
+        ChangeLocation(locations[currentScene]);
+    }
+
+    void Update () {
+        if (currentScene >= locations.Length) {
+            currentScene = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ChangeLocation(locations[currentScene]);
+            currentScene++;
+        }
+
+
+    }
+
+    void ChangeLocation(GameObject target)
+    {
+
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z - 10f);
+    }
+}
