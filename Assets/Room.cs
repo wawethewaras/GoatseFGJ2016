516a8ab81c598c9e2316a1c63db2d2f3db642df6
Oskiii <oliukku@gmail.com>
2016-01-29 21:14:56 +0200

    moving between rooms + placeholder art

---
+++ Assets/Room.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour {
+
+	public GameObject roomUp;
+	public GameObject roomDown;
+	public GameObject roomLeft;
+	public GameObject roomRight;
+}
---
+++ Assets/RoomSwitcher.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RoomSwitcher : MonoBehaviour {
+
+	public GameObject currentRoom;
+
+	public Button upButton;
+	public Button downButton;
+	public Button leftButton;
+	public Button rightButton;
+
+	public void MoveUp(){
+		GameObject pRoom = currentRoom.GetComponent<Room> ().roomUp;
+		Camera.main.transform.position = new Vector3(pRoom.transform.position.x, pRoom.transform.position.y, Camera.main.transform.position.z);
+		currentRoom = currentRoom.GetComponent<Room> ().roomUp;
+	}
+
+	public void MoveDown(){
+		GameObject pRoom = currentRoom.GetComponent<Room> ().roomDown;
+		Camera.main.transform.position = new Vector3(pRoom.transform.position.x, pRoom.transform.position.y, Camera.main.transform.position.z);
+		currentRoom = currentRoom.GetComponent<Room> ().roomDown;
+	}
+
+	public void MoveLeft(){
+		GameObject pRoom = currentRoom.GetComponent<Room> ().roomLeft;
+		Camera.main.transform.position = new Vector3(pRoom.transform.position.x, pRoom.transform.position.y, Camera.main.transform.position.z);
+		currentRoom = currentRoom.GetComponent<Room> ().roomLeft;
+	}
+
+	public void MoveRight(){
+		GameObject pRoom = currentRoom.GetComponent<Room> ().roomRight;
+		Camera.main.transform.position = new Vector3(pRoom.transform.position.x, pRoom.transform.position.y, Camera.main.transform.position.z);
+		currentRoom = currentRoom.GetComponent<Room> ().roomRight;
+	}
+
+	public void ChangeRoom(GameObject room){
+		currentRoom = room;
+	}
+
+}
