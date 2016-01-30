2d1678b80e6bd27f9d65c7b02b56677e177774d8
Oskiii <oliukku@gmail.com>
2016-01-30 22:45:01 +0200

    losegame + credits

---
+++ Assets/LoseGame.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoseGame : MonoBehaviour {
+
+	public Text loseText;
+	public GameObject loseGameScreen;
+	private bool gameLost = false;
+	private float loseGameTime;
+
+	void Start (){
+		//set timescale to 1 IMPORTANT FOR RESTARTING GAME
+		Time.timeScale = 1;
+		loseGameTime = 0;
+	}
+
+	public void LoseTheGame(){
+		//enable end screen
+		loseGameScreen.SetActive (true);
+		gameLost = true;
+		//pause game in bg
+		Time.timeScale = 0;
+		loseText.text = "You're the worst satanist ever. Jesus still loves you.";
+	}
+
+	void Update (){
+		//if game is lost, enable going to credits
+		if (gameLost) {
+			loseGameTime += Time.deltaTime;
+			if (loseGameTime > 0f && Input.GetMouseButtonDown (0)) {
+				Time.timeScale = 1;
+				SceneManager.LoadScene ("credits");
+			}
+		}
+	}
+
+}
+
